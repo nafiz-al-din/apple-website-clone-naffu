7eee14a231db396a68f3702107572f15b90c7b02
nafiz al din <ndin@thetitantech.com>
2024-09-11 17:17:31 +0600

    working on hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { heroVideo, smallHeroVideo } from '@/utils'
 
 const Hero = () => {
+  useGSAP(() => {
+    gsap.to('#hero-title', { opacity: 1, delay: 1.5, animationDuration: 1, ease: 'power3.in' })
+  }, [])
+
+  const [videoSrc, setVideoSrc] = useState(heroVideo)
+
+  useEffect(() => {
+    const resizeVideoSrc = () => {
+      if (window.innerWidth < 760) {
+        setVideoSrc(smallHeroVideo)
+      } else {
+        setVideoSrc(heroVideo)
+      }
+    }
+    window.addEventListener('resize', () => resizeVideoSrc());
+
+    return () => {
+      window.removeEventListener('resize', () => resizeVideoSrc());
+    }
+  }, [])
+  
   return (
-    <div>
-        Hero
-    </div>
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6 w-full flex-center flex-col">
+        <p id='hero-title' className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video muted autoPlay playsInline={true} key={videoSrc} controls={false} className='pointer-events-none'>
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </section>
   )
 }
 
